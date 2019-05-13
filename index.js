e96c530b26979039a6cde08019c6ed50d566af4c
Giovanny Franco <giovannyfranco@mattelsa.net>
2019-05-13 17:56:03 -0500

    Print list of courses and create file with data of student and course

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//Import filesystem library
+const fs = require('fs');
+
 /**
  * Array with offered courses
  */
@@ -27,9 +30,27 @@ let courses = [
         price: '700'
     }
 ];
+
+let createFile = (data) => {
+    let text = `El alumno ${data.student} con documento de identificación número ${data.number}
+    \n se ha inscrito al curso con ID ${data.id} y nombre ${data.course}
+    \n que tiene una duración de ${data.duration} y un valor de ${data.price}.`;
+    fs.writeFile(`${data.id}-${data.course}-${data.student}.txt`, text, (err) =>{
+        if (err) throw (err);
+        console.log('Se ha generado el archivo.');
+    });
+}
 /**
  * Function to search for a course according to an id
  */
 let id = '4';
 let searchedCourse = courses.find( course => course.id == id );
-console.log(searchedCourse);
+
+/**
+ * Function to list all course
+ */
+courses.forEach((course, index) => {
+    setTimeout(function(){
+        console.log(`Id: ${course.id}, Nombre: ${course.name}, Duración: ${course.duration}, Valor: ${course.price}`);
+    }, (2000 * (index + 1)));
+});
