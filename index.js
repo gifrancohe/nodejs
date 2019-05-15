53a280d7bbf63e3fe0ba69c9eddd6ed2366535f4
Giovanny Franco <giovannyfranco@mattelsa.net>
2019-05-15 17:17:59 -0500

    Enrolled in the course

--- index.js
+++ index.js
@@ -32,25 +32,69 @@ let courses = [
 ];
 
 let createFile = (data) => {
-    let text = `El alumno ${data.student} con documento de identificación número ${data.number}
-    \n se ha inscrito al curso con ID ${data.id} y nombre ${data.course}
-    \n que tiene una duración de ${data.duration} y un valor de ${data.price}.`;
-    fs.writeFile(`${data.id}-${data.course}-${data.student}.txt`, text, (err) =>{
+    let text = `The person ${data.person} with identification number ${data.number},
+    \n has enrolled in the course with ID ${data.id} and name ${data.course},
+    \n which has a duration of ${data.duration} hours and a value of $${data.price}.`;
+    console.log(text);
+    fs.writeFile(`${data.id}-${data.course}-${data.person}.txt`, text, (err) =>{
         if (err) throw (err);
-        console.log('Se ha generado el archivo.');
+        console.log('The file was generated correctly.');
     });
 }
+
+const options = {
+    id: {
+        demand: true,
+        alias: 'i'
+    },
+    name: {
+        demand: true,
+        alias: 'n'
+    },
+    document: {
+        demand: true,
+        alias: 'd'
+    }
+};
+
+const argv = require('yargs')
+.command('inscribir', '============== Start of the registration process ==============', options)
+.argv
+
+
+
 /**
  * Function to search for a course according to an id
  */
-let id = '4';
-let searchedCourse = courses.find( course => course.id == id );
-
-/**
- * Function to list all course
- */
-courses.forEach((course, index) => {
-    setTimeout(function(){
-        console.log(`Id: ${course.id}, Nombre: ${course.name}, Duración: ${course.duration}, Valor: ${course.price}`);
-    }, (2000 * (index + 1)));
-});
+let courseId = argv.id; 
+if(courseId) {
+    console.log("Searching the course ...");
+    let searchedCourse = courses.find( course => course.id == courseId );
+    if(searchedCourse === undefined) {
+        console.log("Alert: The identifier entered does not correspond with any of our courses.");
+        console.log("============== List of Courses ==============");
+        courses.forEach((course, index) => {
+            console.log(`ID: ${course.id}, Name: ${course.name}, Duration: ${course.duration}, Price: ${course.price}`);
+        });
+    }else {
+        let data = {
+            person: argv.name,
+            number: argv.document,
+            id: searchedCourse.id,
+            course: searchedCourse.name,
+            duration: searchedCourse.duration,
+            price: searchedCourse.price
+        };
+        createFile(data);
+        console.log("¡Congratulations you have enrolled in the course correctly!");
+    }
+}else {
+    /**
+     * Function to list all course
+     */
+    courses.forEach((course, index) => {
+        setTimeout(function(){
+            console.log(`ID: ${course.id}, Name: ${course.name}, Duration: ${course.duration}, Price: ${course.price}`);
+        }, (2000 * (index + 1)));
+    });
+}
